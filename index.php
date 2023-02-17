96423fc2692b014d313840e611e679ee7341e310
“Francesca <dascanio.francesca@gmail.com>
2023-02-17 14:46:52 +0100

    Inizio esercizio

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+    $paragraph = '';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,6 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP Badwords</title>
     </head>
+
     <body>
         <h1>
             Prova2
